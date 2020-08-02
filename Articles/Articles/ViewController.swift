85cfb885c0f1fc4cc7b76ee8a2e6d2909b1bc264
Narasimhaiah Kolli <kadnnaras@gmail.com>
2020-08-02 15:28:48 +0530

    Story board changes

--- Articles/Articles/ViewController.swift
+++ Articles/Articles/ViewController.swift
@@ -13,8 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "Articles"
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell(style: .default, reuseIdentifier: "identifier")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+}
